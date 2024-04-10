1c4318980f5579ff7200712c10b4ae1e46d243e2
Cezar Augusto Mezzalira <cezar.mezzalira@docente.pr.senac.br>
2024-04-10 19:25:43 -0300

    feat: correção exercício 08

--- src/Exercicio08.java
+++ src/Exercicio08.java
@@ -30,21 +30,33 @@ public class Exercicio08 {
     // 5 - Apresentar o percentual correspondente de votos válidos em relação à
     // quantidade de eleitores;
     double percentualVotosValidos = votosValidos * 100 / totalEleitores;
-    System.out.println("Percentual votos validos: " + percentualVotosValidos);
+    
     // 6 - Apresentar o percentual correspondente de votos válidos do candidato A em
     // relação à quantidade de eleitores;
-
+    final double percentualVotosCandidato1 = votosCandidato1 * 100 / totalEleitores;
+    
     // 7 - Apresentar o percentual correspondente de votos válidos do candidato B em
     // relação à quantidade de eleitores;
-
+    final double percentualVotosCandidato2 = votosCandidato2 * 100 / totalEleitores;
+    
     // 8 - Apresentar o percentual correspondente de votos válidos do candidato C em
     // relação à quantidade de eleitores;
-
+    final double percentualVotosCandidato3 = votosCandidato3 * 100 / totalEleitores;
+    
     // 9 - Apresentar o percentual correspondente de votos nulos em relação à
     // quantidade de eleitores;
+    final double percentualVotosNulos = votosNulos * 100 / totalEleitores;
 
     // 10 - Ao final, apresentar o percentual correspondente de votos em branco em
     // relação à quantidade de eleitores.
+    final double percentualVotosEmBranco = votosEmBranco * 100 / totalEleitores;
+
+    System.out.println("Percentual votos validos: " + percentualVotosValidos);
+    System.out.println("Percentual de votos do candidato 1: " + percentualVotosCandidato1);
+    System.out.println("Percentual de votos do candidato 2: " + percentualVotosCandidato2);
+    System.out.println("Percentual de votos do candidato 3: " + percentualVotosCandidato3);
+    System.out.println("Percentual de votos nulos: " + percentualVotosNulos);
+    System.out.println("Percentual de votos em branco: " + percentualVotosEmBranco);
 
     scanner.close();
   }
